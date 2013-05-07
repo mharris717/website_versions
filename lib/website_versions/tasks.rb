3b14920dce20a34ae7f98fc4c7d38851ac61024d
Mike Harris <mharris717@gmail.com>
2013-05-07 13:09:53 -0400

    moved some tasks around

--- lib/website_versions/tasks.rb
+++ lib/website_versions/tasks.rb
@@ -16,9 +16,16 @@ def run_s3cmd(subcmd, *args)
   puts "Error running command" unless result
 end
 
-namespace :website_versions do
+namespace :versions do
   namespace :s3 do
-    task :setup => [:create_bucket, :create_website, :create_policy, :deploy]
+    task :setup => [:write_creds, :create_bucket, :create_website, :create_policy, :deploy]
+
+    task :write_creds do
+      if !FileTest.exist?("~/.s3cfg")
+        body = "[default]\naccess_key=#{ENV['S3_ACCESS_KEY_ID']}\nsecret_key=#{ENV['S3_SECRET_ACCESS_KEY']}"
+        File.create "~/.s3cfg",body
+      end
+    end
 
     task :create_bucket do
       puts "creating #{ENV['BUCKET_URL']}"
@@ -38,18 +45,30 @@ namespace :website_versions do
     end
 
     task :deploy do
-      run_s3cmd "sync", "--delete-removed", "tmp/website/build/", ENV['BUCKET_URL']
+      if ENV['REF'] == 'edge'
+        run_s3cmd "sync", "--delete-removed", "build/", ENV['BUCKET_URL']
+      else
+        run_s3cmd "sync", "--delete-removed", "tmp/website/build/", ENV['BUCKET_URL']
+      end
     end
   end
 
-  task :build do
+  task :setup_vars do
     ref = ENV['REF']
     bucket = WebsiteVersions.bucket_for_tag(ref)
     ENV['BUCKET_URL'] = "s3://#{bucket}"
-    WebsiteVersions.build_ref(ref)
   end
 
-  task :deploy => [:build, 's3:setup']
+  task :build_site => [:setup_vars] do
+    WebsiteVersions.build_ref(ENV['REF'])
+  end
+
+  task :set_edge_ref do
+    ENV['REF'] = 'edge'
+  end
+
+  task :deploy_version => [:build_site, 's3:setup']
+  task :deploy_edge => [:build,:set_edge_ref, :setup_vars,'s3:setup']
 end
 
 def bucket_policy
